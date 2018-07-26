136b67f9af81a530b6bef866568d30836260f4a2
Chris Coronado <cbcoronado@live.com>
2018-07-26 10:55:34 -0400

    Added test project

---
+++ SecureXWebApp/SecureXTest/UnitTest.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using FluentAssertions;
+using SecureXWebApp;
+
+namespace SecureXTest
+{
+    public class UnitTest
+    {
+        // CC: remove later when we have real unit tests
+        [Fact]
+        public void DummyTest()
+        {
+            var dummy = true;
+            Assert.True(dummy);
+        }
+    }
+}
